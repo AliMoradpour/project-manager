f5e19569378f38afaa783e1b88e688ab76175582
AliMoradpour <mr.mrdp@yahoo.com>
2026-06-13 22:28:47 +0330

    format all response messages

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -7,42 +7,79 @@ import {
   Param,
   Delete,
   Query,
+  HttpStatus,
+  Res,
 } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import ProjectStatusEnum from './enums/projectStatusEnum';
+import express from 'express';
 
 @Controller('projects')
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
   @Post()
-  create(@Body() createProjectDto: CreateProjectDto) {
-    return this.projectsService.create(createProjectDto);
+  async create(
+    @Res() res: express.Response,
+    @Body() createProjectDto: CreateProjectDto,
+  ) {
+    const createProject = await this.projectsService.create(createProjectDto);
+
+    return res.status(HttpStatus.CREATED).json({
+      stausCode: HttpStatus.CREATED,
+      data: createProject,
+      message: 'Project Created',
+    });
   }
 
   @Get()
-  findAll(
+  async findAll(
+    @Res() res: express.Response,
     @Query('status') status?: ProjectStatusEnum,
     @Query('limit') limit: number = 10,
     @Query('page') page: number = 1,
   ) {
-    return this.projectsService.findAll(status, limit, page);
+    const projects = await this.projectsService.findAll(status, limit, page);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      data: projects,
+      message: 'Data Founded',
+    });
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.projectsService.findOne(+id);
+  async findOne(@Res() res: express.Response, @Param('id') id: string) {
+    const foundedProject = await this.projectsService.findOne(+id);
+    return res.status(HttpStatus.FOUND).json({
+      statusCode: HttpStatus.FOUND,
+      data: foundedProject,
+      message: 'Project Founded',
+    });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto) {
-    return this.projectsService.update(+id, updateProjectDto);
+  async update(
+    @Res() res: express.Response,
+    @Param('id') id: string,
+    @Body() updateProjectDto: UpdateProjectDto,
+  ) {
+    await this.projectsService.update(+id, updateProjectDto);
+
+    return res.status(HttpStatus.ACCEPTED).json({
+      statusCode: HttpStatus.ACCEPTED,
+      message: 'Project Updated',
+    });
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.projectsService.remove(+id);
+  async remove(@Res() res: express.Response, @Param('id') id: string) {
+    await this.projectsService.remove(+id);
+    return res.status(HttpStatus.ACCEPTED).json({
+      statusCode: HttpStatus.ACCEPTED,
+      message: 'Project Deleted Successfuly',
+    });
   }
 }
