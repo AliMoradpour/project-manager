3bb9b703171e1c598089a65f8a57a57ae87fdee9
AliMoradpour <mr.mrdp@yahoo.com>
2026-06-24 09:26:57 +0330

    project dto

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,7 +1,11 @@
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import ProjectStatusEnum from '../enums/projectStatusEnum';
 
 export class CreateProjectDto {
+  @IsNotEmpty({ message: 'نام پروژه نمیتواند خالی باشد' })
+  @IsString({ message: 'مقدار پروژه باید حروف باشد' })
   name!: string;
 
+  @IsEnum(ProjectStatusEnum, { message: 'مقدار وضعیت پروژه نا معتبر است' })
   status?: ProjectStatusEnum;
 }
